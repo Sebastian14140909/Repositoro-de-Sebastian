1f38c8e3132133655d6ae8449125b03fdd451063
Sebastian14140909 <133933733+Sebastian14140909@users.noreply.github.com>
2023-06-21 20:27:37 -0600

    Add files via upload

---
+++ JavaApplication3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author LABORATORIO 04
+ */
+public class JavaApplication3 {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Perro bobby = new Perro ();
+        bobby.setNombre("Bobby");
+        bobby.setRaza("Chihuahua");
+        
+    }
+    
+}
